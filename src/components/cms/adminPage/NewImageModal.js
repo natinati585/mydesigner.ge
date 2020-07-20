308309da56ff4c8d9369ce9a223faa78e34efa43
bosto <bostonat10@gmail.com>
2020-07-20 14:32:53 +0400

    Add og-s for facebook, linkedin etc.

--- src/components/cms/adminPage/NewImageModal.js
+++ src/components/cms/adminPage/NewImageModal.js
@@ -42,9 +42,7 @@ const ImagesCreateForm = Form.create({name: 'form_in_modal'})(
         handleChange = info => {
             if (info.file.status === 'uploading') {
                 this.setState({loading: true});
-                return;
-            }
-            if (info.file.status === 'done') {
+            } else {
                 // Get this url from response in real world.
                 getBase64(info.file.originFileObj, imageUrl =>
                     this.setState({
@@ -60,6 +58,7 @@ const ImagesCreateForm = Form.create({name: 'form_in_modal'})(
         };
 
         render() {
+            console.log('imageUrl', this.state.imageUrl)
             const projects = this.props.projects;
             const projectsOrganised = [];
 
@@ -94,7 +93,7 @@ const ImagesCreateForm = Form.create({name: 'form_in_modal'})(
                                 listType="picture-card"
                                 className="avatar-uploader"
                                 showUploadList={false}
-                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                                 beforeUpload={beforeUpload}
                                 onChange={this.handleChange}
                             >
--- src/components/cms/adminPage/NewProjectModal.js
+++ src/components/cms/adminPage/NewProjectModal.js
@@ -33,9 +33,7 @@ const ProjectsCreateForm = Form.create({name: 'form_in_modal'})(
         handleChange = info => {
             if (info.file.status === 'uploading') {
                 this.setState({ loading: true });
-                return;
-            }
-            if (info.file.status === 'done') {
+            }else  {
                 // Get this url from response in real world.
                 getBase64(info.file.originFileObj, imageUrl =>
                     this.setState({
@@ -93,7 +91,7 @@ const ProjectsCreateForm = Form.create({name: 'form_in_modal'})(
                                 listType="picture-card"
                                 className="avatar-uploader"
                                 showUploadList={false}
-                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                                 beforeUpload={beforeUpload}
                                 onChange={this.handleChange}
                             >
